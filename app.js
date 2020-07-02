8ccdde175b9c6e82b98cbde6365da3da836bef33
DevShikhar <shikhar1481997@gmail.com>
2020-07-02 14:48:46 +0530

    app.js modify

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const campRoutes      =   require("./routes/campgrounds"),
 
 
 require('dotenv').config();  
-var url = "mongodb+srv://shikhar_user1:"+ process.env.DBKEY +"@cluster0.mtyig.mongodb.net/Cluster0?retryWrites=true&w=majority"
+var url = "mongodb+srv://shikhar_user1:yalpcamp@cluster0.mtyig.mongodb.net/Cluster0?retryWrites=true&w=majority"
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"))
